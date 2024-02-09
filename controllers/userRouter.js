6b338f8f7497806c7e61ad674906602452667755
Thapasya-s <thapasyasajee@gmail.com>
2024-02-09 11:28:22 +0530

    encrypted

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,14 +1,41 @@
-const express=require("express")
-const router=express.Router()
-const userModel=require("../models/userModel")
+const express = require("express")
+const router = express.Router()
+const userModel = require("../models/userModel")
+const bcrypt = require("bcryptjs")
+
+hashPasswordGenerator = async (pass) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass, salt)
+}
+
+router.post("/add", async (req, res) => {
+    //let name=req.body.name
+    //let age=req.body.age
+    //let mobile=req.body.mobile
+    //let address=req.body.address
+    //let pin=req.body.pin
+    //let email=req.body.email
+    //let password=req.body.password
+
+    let { data } = { "data": req.body }
+    let password = req.body.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword) => {
+            console.log(hashedPassword)
+            data.password = hashedPassword
+            console.log(data)
+            let user = new userModel(data)
+            let result = user.save()
+            res.json({
+                status: "success"
+            })
+
+        }
+    )
 
 
-router.post("/add",async(req,res)=>{
-    let data=req.body
-    let user=new userModel(data)
-    let result=await user.save()
-    res.json({
-        status:"success"
-    })
+    //let user=new userModel(data)
+    //let result=await user.save()
+    
 })
-module.exports=router
+module.exports = router
